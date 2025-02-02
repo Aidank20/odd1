683a07de478e3ac8c84d747dd714699f18122c64
Aidank20 <aidan.k@westcoast_schools.com>
2025-02-02 09:51:27 +0400

    commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        int diff=a-b;
+        int product=a*b;
+        int quotient=a/b;
+        System.out.println("********Magic********");
+        System.out.println("Sum of a and b is: "+sum);
+        System.out.println("Difference of a and b is: "+diff);
+        System.out.println("Quotient is: "+quotient);
+    }
+}
